db986ea5c3775da16d70b897b62ae29295ee732f
Rajesh K <Rajesh.Kokkeragadda@expenseondemand.com>
2021-02-18 07:45:05 +0530

    gg

--- SSTEST/SSTEST/Program.cs
+++ SSTEST/SSTEST/Program.cs
@@ -6,7 +6,7 @@ namespace SSTEST
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!   " );
         }
     }
 }

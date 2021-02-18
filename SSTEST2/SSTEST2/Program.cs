b24d3f08255c107b74ab8b704ebb155db8c74a43
Rajesh K <Rajesh.Kokkeragadda@expenseondemand.com>
2021-02-18 08:42:16 +0530

    Added New

---
+++ SSTEST2/SSTEST2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SSTEST2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
